91b975aa642889d1094d9601e8d029895d3c16be
Great Adams <kingifean@gmail.com>
2023-08-26 18:48:53 +0100

    Reset password page

--- pages/auth/reset.tsx
+++ pages/auth/reset.tsx
@@ -1,46 +1,33 @@
-import { GET_USER, LOGIN_USER } from '@/apollo/auth';
+import { RESET_PASSWORD } from '@/apollo/auth';
 import { useMutation } from '@apollo/client';
 import React, { useEffect, useState } from 'react';
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 import { message } from 'antd';
-import { setCookie } from 'cookies-next';
-import { deleteCookie } from 'cookies-next';
 import Link from 'next/link';
-import SocialAuth from '@/components/SocialAuth';
+import { useRouter } from 'next/router';
 
 function Reset() {
 
-  const [email, setEmail] = useState("")
+  const [otp, setotp] = useState(0)
   const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
   const [messageApi, contextHolder] = message.useMessage();
   const [show, setShow] = useState(false)
 
+  const router = useRouter()
 
-  useEffect(() => {
-    deleteCookie("token")
-    window.localStorage.clear()
-  }, [])
-
-  const [login, { loading }] = useMutation(LOGIN_USER, {
+  const [resetPassword, { loading }] = useMutation(RESET_PASSWORD, {
     variables: {
-      email,
+      otp,
       password
     },
     onCompleted: (data) => {
       console.log(data)
-      setCookie('token', data.login.token);
       messageApi.open({
         type: 'success',
-        content: 'Logged in successfully!',
+        content: 'Password Reset successful!!!',
       });
-      if (data.login.accountType === "VENDOR") {
-        window.location.href = "/vendor/listing"
-        // router.push("/vendor/listing")
-      } else if (data.login.accountType === "GUEST") {
-        // router.push("/guest/history")
-        window.location.href = "/guest/history"
-
-      }
+      router.push('/auth/login')
     },
     onError: (error) => {
       messageApi.open({
@@ -50,28 +37,48 @@ function Reset() {
     }
   })
 
+  const reset = () => {
+    if (!otp) {
+      messageApi.open({
+        type: 'error',
+        content: 'Add OTP',
+      });
+      return
+    }
+
+    if ( password !== confirmPassword) {
+      messageApi.open({
+        type: 'error',
+        content: 'Passwords must match!!!',
+      });
+      return
+    }
+
+    resetPassword()
+  }
+
   return (
     <>
       {contextHolder}
       <main>
-        <div className='lg:flex justify-center'>
+        <div className='lg:flex justify-center mt-14'>
 
           <div className='lg:mx-20 lg:w-1/2 my-auto'>
             <div className='text-center my-4'>
-              <h5 className='font-bold text-xl'>Login</h5>
-              <p>Please enter your valid details below</p>
+              <h5 className='font-bold text-xl'>Reset Password</h5>
+              <p>OTP code has been sent to your email</p>
             </div>
-            <div className="w-full space-y-6 text-gray-600 sm:max-w-md">
+            <div className="w-full text-gray-600 sm:max-w-md">
               <div className="p-4 py-6 space-y-8 sm:p-6 sm:rounded-lg">
                 <div>
                   <label className="font-medium">
-                    Email
+                    OTP
                   </label>
                   <input
-                    type="email"
+                    type="number"
                     required
                     className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => setotp(parseInt(e.target.value))}
                   />
                 </div>
                 <div>
@@ -102,11 +109,37 @@ function Reset() {
                     }
                   </div>
                 </div>
+                <div className='w-full'>
+                    <label className="font-medium">
+                      Confirm Password
+                    </label>
+                    <input
+                      type={show ? "text" : "password"}
+                      required
+                      className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    {show ? 
+
+                      (
+                        <>
+                          <EyeSlashIcon className='w-5 cursor-pointer relative right-3 top-[-30px] float-right' onClick={() => setShow(!show)} />
+                        </>
+                      ) :
+
+                      (
+                        <>
+                          <EyeIcon className='w-5 cursor-pointer relative right-3 top-[-30px] float-right' onClick={() => setShow(!show)} />
+                        </>
+                      )
+
+                    }
+                  </div>
                 <button
                   className="w-full px-4 py-2 text-white font-medium bg-primaryColor rounded-lg"
-                  onClick={() => login()}
+                  onClick={() => reset()}
                 >
-                  {loading ? "Loading.." : "Login"}
+                  {loading ? "Loading.." : "Reset password"}
                 </button>
                 <div className=' text-center lg:flex justify-between text-sm'>
                   <p>Don&apos;t have an account? <Link className='text-primaryColor' href={"/auth/signup?page=vendor"}>Signup as Vendor</Link></p>
